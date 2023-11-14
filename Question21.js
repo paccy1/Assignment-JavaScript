38dada052efc395085043132b8ee2644028040ed
UWINGABIRE Pascaline <Uwingpaccy@gmail.com>
2023-11-14 17:05:32 +0200

    countEvenOdd

---
+++ Question21.js
@@ -0,0 +1,8 @@
+function displayOddNumbers() {
+    for (let i = 1; i <= 100; i += 2) {
+        console.log(i);
+    }
+}
+
+// Call the function to display odd numbers between 1 and 100
+displayOddNumbers();
