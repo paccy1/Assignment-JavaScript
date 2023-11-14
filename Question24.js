0db1a6628e0a461845550c5d0b1e3f8b91f3593b
UWINGABIRE Pascaline <Uwingpaccy@gmail.com>
2023-11-14 17:18:37 +0200

    q22 up to q26

---
+++ Question24.js
@@ -0,0 +1,7 @@
+function findBiggerNumber(number1, number2) {
+    return (number1 > number2) ? number1 : number2;
+}
+
+// Example: Find the bigger number between 23 and 12
+var result = findBiggerNumber(23, 12);
+console.log("Bigger number:", result);
---
+++ Question25.js
@@ -0,0 +1,7 @@
+function convertToUpperCase(inputString) {
+    return inputString.toUpperCase();
+}
+
+// Example: Convert "Hello World" to uppercase
+var result = convertToUpperCase("Hello World");
+console.log("Uppercase string:", result);
